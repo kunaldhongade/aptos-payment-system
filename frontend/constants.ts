84037aa5ae1e7e36c212cfc6f4855dc05cc9ff4f
kunaldhongade <kunaldhongade1@gmail.com>
2024-10-07 19:15:20 +0530

    Update: Smart contract address update

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,5 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
-export const MODULE_ADDRESS = "0xaf12ecd9cf6578db88443a384a2d028e67ecda31cf0f441072d22201da3c0072";
+export const MODULE_ADDRESS = "0x355afa735ffb0e2e6a76ee3242bb17ffd937d2677e1aa64161c84970dd2ab40e";
 export const CREATOR_ADDRESS = import.meta.env.VITE_COLLECTION_CREATOR_ADDRESS;
 export const COLLECTION_ADDRESS = import.meta.env.VITE_COLLECTION_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
